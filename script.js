a63b27573dd11976f0145cff457f928daf002998
jose moises <106701602+josemoises2007@users.noreply.github.com>
2023-10-07 17:57:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const progress = document.getElementById('loading-progress');
-const image = document.getElementById('loading-image');
+document.addEventListener('DOMContentLoaded', function() {
+  const progress = document.getElementById('loading-progress');
+  const image = document.getElementById('loading-image');
 
-function simulateLoading() {
-  progress.value += 0.9;
+  function simulateLoading() {
+    if (progress) {
+      progress.value += 0.9;
 
-  if (progress.value < progress.max) {
-    requestAnimationFrame(simulateLoading);
-  } else {
-    image.style.display = 'block';
-    setTimeout(function() {
-      fadeOut();
-    }, 3000);
+      if (progress.value < progress.max) {
+        requestAnimationFrame(simulateLoading);
+      } else {
+        image.style.display = 'block';
+        setTimeout(function() {
+          fadeOut();
+        }, 3000);
+      }
+    }
   }
-}
 
-function fadeOut() {
-  const container = document.querySelector('.loading-container');
-  container.style.opacity = '0';
-  container.style.transition = 'opacity 1s ease-in-out';
+  function fadeOut() {
+    const container = document.querySelector('.loading-container');
+    if (container) {
+      container.style.opacity = '0';
+      container.style.transition = 'opacity 1s ease-in-out';
 
-  setTimeout(function() {
-    container.style.display = 'none';
-  }, 3000);
-}
+      setTimeout(function() {
+        container.style.display = 'none';
+      }, 3000);
+    }
+  }
 
-simulateLoading();
+  simulateLoading();
+});
